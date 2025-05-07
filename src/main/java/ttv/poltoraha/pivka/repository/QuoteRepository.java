a98da63f0b9daf479daf5f9dbab5ce199b36fd7b
aveasura <aveasura@gmail.com>
2025-05-07 20:09:36 +0300

    [refactor] Сохранить новые цитаты напрямую, используя QuoteRepository вместо ReaderRepository.

---
+++ src/main/java/ttv/poltoraha/pivka/repository/QuoteRepository.java
@@ -0,0 +1,6 @@
+package ttv.poltoraha.pivka.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ttv.poltoraha.pivka.entity.Quote;
+
+public interface QuoteRepository extends CrudRepository<Quote, Integer> {}
