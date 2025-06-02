5df8573b6a221cacb0b5002310fc1881089abb2c
Даниил <dankatchernyh@ya.ru>
2025-06-02 19:25:30 +0300

    add logging it authoe service and controller

--- src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
+++ src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
@@ -2,7 +2,11 @@ package ttv.poltoraha.pivka.serviceImpl;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -27,9 +31,11 @@ public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
     private final AuthorMapper authorMapper;
 
-    // todo как будто надо насрать всякими мапперами
+    private static final Logger logger = LoggerFactory.getLogger(AuthorServiceImpl.class);
+
     @Override
     public void create(AuthorDto authorDto) {
+        logger.debug("Проверка существования автора: {}", authorDto.getFullName());
         authorRepository.findAuthorByFullName(authorDto.getFullName()).ifPresent(
                 author -> {
                     throw new AuthorAlreadyExistsException(String.format(
@@ -37,27 +43,37 @@ public class AuthorServiceImpl implements AuthorService {
                     );
                 }
         );
-
+        logger.debug("Создание нового автора");
         Author savedAuthor = authorMapper.mapFromDtoToEntity(authorDto);
         authorRepository.save(savedAuthor);
+        logger.info("Автор сохранен в БД с ID: {}", savedAuthor.getId());
     }
 
     @Override
     public void delete(Integer id) {
+        logger.debug("Проверка существования автора с ID: {}", id);
         Author obtainedAuthor = getOrThrow(id);
+        logger.debug("Удаление нового автора");
         authorRepository.deleteById(obtainedAuthor.getId());
+        logger.info("Автор удален из БД с ID: {}", id);
     }
 
     @Override
     public void addBooks(Integer id, List<Book> books) {
+        logger.debug("Проверка существования автора с ID: {}", id);
         val author = getOrThrow(id);
+        logger.debug("Добавление книг для автора с ID: {}", id);
         author.getBooks().addAll(books);
+        logger.info("Книги добавлены в список книг автора с ID: {}", id);
     }
 
     @Override
     public void addBook(Integer id, Book book) {
+        logger.debug("Проверка существования автора с ID: {}", id);
         val author = getOrThrow(id);
+        logger.debug("Добавление книги для автора с ID: {}", id);
         author.getBooks().add(book);
+        logger.info("Книга добавлены в список книг автора с ID: {}", id);
     }
 
     @Override
@@ -70,6 +86,9 @@ public class AuthorServiceImpl implements AuthorService {
 
     private Author getOrThrow(Integer id) {
         return authorRepository.findById(id)
-                .orElseThrow(() -> new AuthorNotFoundException("Author with id = " + id + " not found"));
+                .orElseThrow(() -> {
+                    logger.warn("Не найден автор с ID: {}", id);
+                    throw new AuthorNotFoundException("Author with id = " + id + " not found");
+                });
     }
 }
